ff721f145f0e022aca316d2c7292e9cb8f6b800f
Shambhavi <Shambhavi@TYSS-Shambhavi>
2023-12-19 11:10:14 +0530

    push

---
+++ jenkinsMavenProject/src/test/java/jenkinsMavenProject/jenkinsTest.java
@@ -0,0 +1,14 @@
+package jenkinsMavenProject;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class jenkinsTest {
+	
+	@Test
+	public void sample()
+	{
+		Reporter.log("added....");
+	}
+
+}
